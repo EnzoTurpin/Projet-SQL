4612549eeac957d344082e917feb0706bec58233
Enzo Turpin <enzoturpin35@orange.fr>
2025-04-09 09:13:38 +0200

    Added a cors + test of the connexion from Angular

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { UserService, User } from './services/user.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'angular-app';
+export class AppComponent implements OnInit {
+  users: User[] = [];
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe((res) => {
+      this.users = res.data ?? res; // s’adapte à ton retour Laravel
+    });
+  }
 }
--- angular-app/src/app/services/user.service.ts
+++ angular-app/src/app/services/user.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface User {
+  _id: string;
+  name: string;
+  email: string;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  private apiUrl = 'http://localhost:8000/api/users';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getUsers(): Observable<{ data: User[] }> {
+    return this.http.get<{ data: User[] }>(this.apiUrl);
+  }
 }
