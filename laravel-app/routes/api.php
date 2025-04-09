383559e1646b91adc8f4b5aa141821801d38c95a
Enzo Turpin <enzoturpin35@orange.fr>
2025-04-09 15:58:59 +0200

    Fixed the routes with the middlewares

--- laravel-app/routes/api.php
+++ laravel-app/routes/api.php
@@ -8,76 +8,56 @@ use App\Http\Controllers\GlassController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\AuthController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+// Routes publiques
+Route::post('/register', [UserController::class, 'store']);
+Route::post('/login', [AuthController::class, 'login']);
 
-// Routes users
-Route::get('users', [UserController::class, 'index']);
-Route::get('/users/{id}', [UserController::class, 'show']);
-
-Route::middleware('auth:sanctum')->group(function () {
-    Route::put('/users', [UserController::class, 'update']);
-    Route::post('/users', [UserController::class, 'store']);
-    Route::delete('/users/{id}', [UserController::class, 'destroy']);
-    Route::post('/users/{id}/ban', [UserController::class, 'ban']);
-});
-
-// Route recipes
-Route::get('recipes', [RecipeController::class, 'index']);
+Route::get('/recipes', [RecipeController::class, 'index']);
 Route::get('/recipes/{id}', [RecipeController::class, 'show']);
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::put('/recipes', [RecipeController::class, 'update']);
-    Route::post('/recipes', [RecipeController::class, 'store']);
-    Route::delete('/recipes/{id}', [RecipeController::class, 'destroy']);
-    Route::post('/recipes/{id}/ban', [RecipeController::class, 'ban']);
-});
-
-// Route ingredients
-Route::get('ingredients', [IngredientController::class, 'index']);
+Route::get('/ingredients', [IngredientController::class, 'index']);
 Route::get('/ingredients/{id}', [IngredientController::class, 'show']);
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::put('/ingredients', [IngredientController::class, 'update']);
-    Route::post('/ingredients', [IngredientController::class, 'store']);
-    Route::delete('/ingredients/{id}', [IngredientController::class, 'destroy']);
-    Route::post('/ingredients/{id}/ban', [IngredientController::class, 'ban']);
-});
-
-// Route glass
-Route::get('glass', [GlassController::class, 'index']);
+Route::get('/glass', [GlassController::class, 'index']);
 Route::get('/glass/{id}', [GlassController::class, 'show']);
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::put('/glass', [GlassController::class, 'update']);
-    Route::post('/glass', [GlassController::class, 'store']);
-    Route::delete('/glass/{id}', [GlassController::class, 'destroy']);
-    Route::post('/glass/{id}/ban', [GlassController::class, 'ban']);
-});
-
-// Route categories
-Route::get('categories', [CategoryController::class, 'index']);
+Route::get('/categories', [CategoryController::class, 'index']);
 Route::get('/categories/{id}', [CategoryController::class, 'show']);
 
+// Routes protégées par auth:sanctum
 Route::middleware('auth:sanctum')->group(function () {
-    Route::put('/categories', [CategoryController::class, 'update']);
+
+    // Authentification
+    Route::get('/me', [UserController::class, 'show']);
+    Route::put('/me', [UserController::class, 'update']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    // Gestion utilisateurs
+    Route::get('/users', [UserController::class, 'index']);
+    Route::delete('/users/{id}', [UserController::class, 'destroy']);
+    Route::post('/users/{id}/ban', [UserController::class, 'ban']);
+
+    // Gestion recettes
+    Route::post('/recipes', [RecipeController::class, 'store']);
+    Route::put('/recipes', [RecipeController::class, 'update']);
+    Route::delete('/recipes/{id}', [RecipeController::class, 'destroy']);
+    Route::post('/recipes/{id}/ban', [RecipeController::class, 'ban']);
+
+    // Gestion ingrédients
+    Route::post('/ingredients', [IngredientController::class, 'store']);
+    Route::put('/ingredients', [IngredientController::class, 'update']);
+    Route::delete('/ingredients/{id}', [IngredientController::class, 'destroy']);
+    Route::post('/ingredients/{id}/ban', [IngredientController::class, 'ban']);
+
+    // Gestion verres
+    Route::post('/glass', [GlassController::class, 'store']);
+    Route::put('/glass', [GlassController::class, 'update']);
+    Route::delete('/glass/{id}', [GlassController::class, 'destroy']);
+    Route::post('/glass/{id}/ban', [GlassController::class, 'ban']);
+
+    // Gestion catégories
     Route::post('/categories', [CategoryController::class, 'store']);
+    Route::put('/categories', [CategoryController::class, 'update']);
     Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
     Route::post('/categories/{id}/ban', [CategoryController::class, 'ban']);
-});
-
-// Auth
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', [AuthController::class, 'user']);
-    Route::post('/logout', [AuthController::class, 'logout']);
 });
